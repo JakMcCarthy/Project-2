78e1114b62c1f02158c3bb681f47c239f2af5ddc
Umbalite <karanbir.dhawan@gmail.com>
2022-06-29 20:18:17 -0400

    SASS 6/29

---
+++ assets/js/script.js
@@ -0,0 +1,123 @@
+window.onload = () => {
+  const tab_switchers = document.querySelectorAll('[data-switcher]');
+  let testGoogle = "";
+  let taskButton = document.querySelector("#generate");
+  let book = [];
+  var markedTitle = [];
+  var activities = [];
+
+  
+  taskButton.addEventListener("click", function () {
+
+    testFetch = fetch('https://www.boredapi.com/api/activity/')
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (response) {
+        activity = response.activity
+        activities.push(activity);
+        localStorage.setItem("bookmarkedActivity", JSON.stringify(activities));
+        document.getElementById("displayText").innerText = activity;
+        activity.toLowerCase(); //converts activity AFTER its been appended for user. 
+        let newStringArray = activity.split(' '); //converts activity string to an array
+        const bannedTerms = ['the', 'a', 'and', 'too', 'or', 'some', 'of', 'an', 'trip', 'on'];//terms that are checked for within the array so they can be removed for better search results
+        newStringArray = newStringArray.filter(e => bannedTerms.includes(e) === false); //e represents all the elements in a list, => returns the values, e => bannedTerms.includes(e) === false is saying, do not return the elements if they are included in banned terms
+        let cleanedSearch = newStringArray.toString(); //convert the array back to a string
+
+        testGoogle = fetch('https://www.googleapis.com/customsearch/v1?key=AIzaSyAKzE32n6Ad0lWIE3hOhOeYWyMbOoQ0PyE&q=' + cleanedSearch + '&cx=6a0eab11f4f52b42f') //the cleaned search is included here
+          .then(function (response) {
+            return response.json();
+          })
+          .then(function (response) {
+            let contentBox = document.getElementsByClassName('contentContainer');
+            let contentBoxId = [];
+            for (i = 0, len = contentBox.length; i < len; i++) {
+              if (contentBox[i].id != '') {
+                contentBoxId.push(contentBox[i].id);
+              }
+            }
+
+            for (i = 0; i < response.items.length; i++) {
+              var linkItems = response.items[i].link;
+              var linkTitles = response.items[i].title;
+              contentBox[i].innerHTML = '<br>' + '<a href=' + linkItems + ' target="_blank">' + linkTitles + '</a></br>';
+              contentBox[i].setAttribute("href", linkItems);
+            }
+
+            document.getElementById("contentContainer").style = "display: contents";
+          })
+      })
+  });
+
+  let saveMark = document.querySelectorAll(".btnSave").forEach(item => {
+    item.addEventListener("click", e => {
+      var link = e.target.parentElement.firstElementChild.getAttribute("href");
+      var title = e.target.parentElement.firstElementChild.textContent;
+      console.log(title);
+      console.log(link);
+      book.push(link);
+      markedTitle.push(title);
+      localStorage.setItem("bookmarked", JSON.stringify(book));
+      localStorage.setItem("bookmarkedTitle", JSON.stringify(markedTitle));
+      console.log("works");
+      display: alert("Taskdded to your bookmarks");
+    })
+  })
+
+  for (let i = 0; i < tab_switchers.length; i++) {
+    const tab_switcher = tab_switchers[i];
+    const page_id = tab_switcher.dataset.tab;
+
+
+
+    tab_switcher.addEventListener('click', () => {
+      document.querySelector('.tabs .tab.is-active').classList.remove('is-active');
+      tab_switcher.parentNode.classList.add('is-active');
+
+      SwitchPage(page_id)
+    });
+  }
+}
+
+function SwitchPage(page_id) {
+  const current_page = document.querySelector('.pages .page.is-active');
+  current_page.classList.remove('is-active');
+
+  const next_page = document.querySelector(`.pages .page[data-page="${page_id}"]`);
+  next_page.classList.add('is-active');
+
+}
+
+
+var loadTask = function() { 
+
+  loadlinks = JSON.parse(localStorage.getItem("bookmarked"));
+  loadtitles = JSON.parse(localStorage.getItem("bookmarkedTitle"));
+  loadActivity = JSON.parse(localStorage.getItem("bookmarkedActivity"));
+
+
+  var bookcontain = document.querySelector(".bookCon");
+
+  for (i = 0; i < loadlinks.length; i++) {
+    var bookLink = document.createElement("tr");
+    var bookItem = loadlinks[i];
+    var bookTitle = loadtitles[i];
+    var bookAct =loadActivity;
+    bookLink.innerHTML = '<td>' + bookAct + '</td>' +'<td>' + '<a href=' + bookItem + ' target="_blank">' + bookTitle + "</a>" + '</td>';
+    bookcontain.append(bookLink);
+  }
+}
+
+//JS FOR SIDEBAR DONE ON 6/27/22 BY KARAN (Delete if works)//
+
+/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+function openNav() {
+  document.getElementById("my Sidebar").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+function closeNav() {
+  document.getElementById("my Sidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+}
